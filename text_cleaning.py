6c05c234b4b9177b8cb663f655051a1e4cf1259e
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2019-01-08 08:30:23 +0200

    checkpoint

--- text_cleaning.py
+++ text_cleaning.py
@@ -1,13 +1,20 @@
-import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-def clean_text(input_text: str) -> str:
+def clean_text(input_text: str) -> list[str]:
     # TODO: Clean input_text
+    # TODO: Lower case the text before processing it
 
-def remove_stop_words(input_text: str) -> str:
+def remove_stop_words(input_text: str) -> list[str]:
     # TODO: Remove stop words from input_text
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(input_text)
 
-def remove_punctuation_marks(input_text: str) -> str:
+    output = [word for word in word_tokens if word not in stop_words]
+    return output
+
+def remove_punctuation_marks(input_text: str) -> list[str]:
     # TODO: Remove punctuation marks from input_text
 
-def remove_additional_blanks(input_text: str) -> str:
+def remove_additional_blanks(input_text: str) -> list[str]:
     # TODO: Remove additional blank spaces from input_text
