a663ea4169b7f36146d8aecfa966ee080c9982c7
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2019-01-08 08:13:33 +0200

    text_cleaning prototype

---
+++ text_cleaning.py
@@ -0,0 +1,13 @@
+import nltk
+
+def clean_text(input_text: str) -> str:
+    # TODO: Clean input_text
+
+def remove_stop_words(input_text: str) -> str:
+    # TODO: Remove stop words from input_text
+
+def remove_punctuation_marks(input_text: str) -> str:
+    # TODO: Remove punctuation marks from input_text
+
+def remove_additional_blanks(input_text: str) -> str:
+    # TODO: Remove additional blank spaces from input_text
