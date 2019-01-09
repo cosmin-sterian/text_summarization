2ee4be5754f7e0399c8838df31db7e93d64ce35f
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2019-01-09 10:19:09 +0200

    task 1 done, I think

--- tema3.py
+++ tema3.py
@@ -1,7 +1,12 @@
 import nltk
+import text_cleaning
+
 
 def main():
-    # TODO
+    sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good. French-Fries"
+    print("".join(text_cleaning.clean_text(sentence)))
+    # TODO: Actual stuff
+
 
 if __name__ == "__main__":
     main()
--- text_cleaning.py
+++ text_cleaning.py
@@ -1,20 +1,28 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
 
-def clean_text(input_text: str) -> list[str]:
+
+def clean_text(input_text: str):
     # TODO: Clean input_text
     # TODO: Lower case the text before processing it
+    input_text = input_text.lower()
+    input_text = remove_punctuation_marks(input_text)
+    input_text = remove_stop_words(input_text)
+    return input_text
 
-def remove_stop_words(input_text: str) -> list[str]:
-    # TODO: Remove stop words from input_text
+
+def remove_stop_words(input_text: str):
+    # Remove stop words from input_text
     stop_words = set(stopwords.words('english'))
     word_tokens = word_tokenize(input_text)
 
     output = [word for word in word_tokens if word not in stop_words]
-    return output
+    return " ".join(output)
 
-def remove_punctuation_marks(input_text: str) -> list[str]:
-    # TODO: Remove punctuation marks from input_text
 
-def remove_additional_blanks(input_text: str) -> list[str]:
-    # TODO: Remove additional blank spaces from input_text
+def remove_punctuation_marks(input_text: str):
+    # Remove punctuation marks from input_text
+    tokenizer = RegexpTokenizer(r'\w+')
+    output = tokenizer.tokenize(input_text)
+    return " ".join(output)
